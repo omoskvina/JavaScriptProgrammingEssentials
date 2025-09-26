7de4a2966d41e002358e21e5f09429efcebdd8b5
omoskvina <moskwinaes@gmail.com>
2025-09-26 11:27:37 -0400

    removed api key empty string

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -4,7 +4,6 @@ function showweatherDetails(event) {
     event.preventDefault();
 
     const city = document.getElementById('city').value;
-    //const apiKey = ''; // Replace 'YOUR_API_KEY' with your actual API key
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(apiUrl)
